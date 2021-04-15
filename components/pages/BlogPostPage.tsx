304340f0cad2a6cf5e033ad1333072a923ea70cd
Brendan McKenzie <hello@brendanmckenzie.com>
2021-04-15 13:26:16 +1000

    bugs

--- components/pages/BlogPostPage.tsx
+++ components/pages/BlogPostPage.tsx
@@ -43,7 +43,7 @@ export const BlogPostPage: React.FC<BlogPost> = ({ post }) => (
             })}
           </p>
         </div>
-        {post.image?.url.split("/")[4] !== "" ? (
+        {post.image?.url && post.image?.url.split("/")[4] !== "" ? (
           <Image src={post.image.url} height={600} width={1200} />
         ) : null}
         <Markdown
