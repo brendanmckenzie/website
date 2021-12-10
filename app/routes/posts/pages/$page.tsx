f20e6f5d8630e2a38b6c41df2672b89182de3618
Brendan McKenzie <hello@brendanmckenzie.com>
2021-12-10 19:00:19 +1100

    sitemap

--- app/routes/posts/pages/$page.tsx
+++ app/routes/posts/pages/$page.tsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import {
   HeadersFunction,
   json,
+  Link,
   LinksFunction,
   LoaderFunction,
   MetaFunction,
---
+++ app/routes/sitemap[.xml]/index.tsx
@@ -0,0 +1,40 @@
+import { LoaderFunction } from "remix";
+import { SitemapStream, streamToPromise } from "sitemap";
+import { client } from "~/pokko";
+import {
+  ListPostsQuery,
+  ListPostsQueryVariables,
+  ListPostsDocument,
+} from "~/pokko/queries";
+
+export const loader: LoaderFunction = async () => {
+  const res = await client.query<ListPostsQuery, ListPostsQueryVariables>({
+    query: ListPostsDocument,
+    fetchPolicy: "network-only",
+    variables: {
+      skip: 0,
+      take: 512,
+    },
+  });
+
+  const smStream = new SitemapStream({
+    hostname: "https://www.brendanmckenzie.com/",
+  });
+
+  smStream.write({ url: "/", changefreq: "weekly", priority: 0.8 });
+
+  res.data.entries?.allPostBase?.nodes.forEach((post) => {
+    smStream.write({
+      url: ["", "posts", post?.date?.substr(0, 4), post?.alias].join("/"),
+      changefreq: "weekly",
+      priority: 1,
+    });
+  });
+
+  smStream.end();
+  const sm = await streamToPromise(smStream);
+
+  return new Response(sm.toString("utf-8"), {
+    headers: { "Content-type": "application/xml" },
+  });
+};
